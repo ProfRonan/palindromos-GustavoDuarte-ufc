a0a2c5a98b619e1a41d7ee456fda289059af7a06
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-05-06 23:41:45 -0300

    Resolvido

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """Main functions"""
+import re
 
 def is_palindrome(string: str) -> bool:
-    b = string.lower().replace(' ', '')
-
-    if b[:] == b[::-1]:
+    b = string.lower()
+    d = re.sub('[.,! s?@:;]', '', b)
+    c = d[::-1]
+    if d == c:
         return True
     else:
         return False
